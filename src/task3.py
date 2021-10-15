fe7d9770c7770f4163806518be43aa1f4211a418
Siba Issa <51207510+SibaIssa@users.noreply.github.com>
2021-10-15 10:30:53 +0300

    Update task3.py

--- src/task3.py
+++ src/task3.py
@@ -1,42 +1,37 @@
 import time
-import inspect 	
-import contextlib 
-import io
+from io import StringIO
+from contextlib import redirect_stdout
+from inspect import signature, getsource
 
-class decorator3:
-    def __init__(self,func):
-        self.func=func
-        self.calls=0
-        self.exce_time=0.0
-
-    def __call__(self, *args, **kwargs):
-        self.calls += 1
-        start = time.time()
+class BestDecor:
+    def __init__(self, func):
+        self.calls = 0 # number of function calls
+        self.func = func # function itself
+        self.executed_time = 0
         
-        with contextlib.redirect_stdout(io.StringIO())as f:
-            self.func(*args, **kwargs)
-        self.exce_time = time.time() - start
-
-        with open ('Output.txt','w') as w:
-            w.write(f"{self.func.__name__} call {self.count} executed in {self.exec_time} sec")
-            w.write('\n')
-            w.write(f"Name:   {self.func.__name__}")
-            w.write('\n')
-            w.write(f"Type:   {type(self.func)}")
-            w.write('\n')
-            w.write(f"Sign:   {inspect.signature(self.func)}")  
-            w.write('\n')            
-            w.write(f"Args:   positional ({self.func.args}) key=worded ({self.func.kwargs})") 
-            w.write('\n')            
-            w.write(f"Doc:    {self.func.__doc__}")
-            w.write('\n')            
-            w.write(inspect.getsource(self.func))
-            w.write('\n')            
-            w.write(f"Output: {self.func(*args, **kwargs)}") 
-            w.write('\n\n')
-
-        if self.calls==4:   #This condition to print the table once all 4 functions are called
-            A=sorted(self.calls.items(),key = lambda x: (x[1],x[0]))
-            print("PROGRAM  |  RANK  |  TIME ELAPSED")
-            for i in range(len(A)):
-                print (A[i][0], '       ',i+1,'      ',A[i][1])                        
+    def __call__(self, *args, **kargs):
+        self.calls += 1
+        start_time = time.time()
+        f = StringIO()
+        with redirect_stdout(f):
+            self.func(*args, **kargs)
+        executed_time = time.time() - start_time
+        self.executed_time = executed_time
+        # Put dumps into one file
+        # if each function need to be in different files
+        # with open(f"stdout_{self.func.__name__}.txt", "w+") as f:
+        with open('stdout.txt', 'a') as file:
+            file.write("{0} call {1} executed in {2} sec\n".format(self.func.__name__ ,self.calls, round(executed_time, 4)))
+            file.write(
+            """
+            Name: {0}\n
+            Type: {1}\n
+            Sign: {2}\n
+            Args: positional {3}\n
+                  key=worded {4}
+            Doc: {5}\n
+            Source: {6}\n
+            Output: {7}\n
+            {8}
+            """.format(self.func.__name__, type(self.func), signature(self.func), args, kargs, self.func.__doc__, getsource(self.func), f.getvalue(), '-'*20))
+        file.close()
